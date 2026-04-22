a716e0fc942b5c31ff48b3dfaa9011a13cf0b76f
Fernando Oriundo <faoriundo@gmail.com>
2026-04-21 22:31:56 -0400

    Started data/centers

--- data/centers.js
+++ data/centers.js
@@ -2,23 +2,46 @@ import { centers } from "../config/mongoCollections.js";
 import { ObjectId } from "mongodb";
 
 const getAllCenters = async () => {
-  // TODO: implement
   const centerCollection = await centers();
   return await centerCollection.find({}).toArray();
 };
 
 const getCenterById = async (id) => {
-  // TODO: implement with validation
-  if (!id || !ObjectId.isValid(id)) throw new Error("Invalid id provided.");
+  if (!id || typeof id !== "string") throw new Error("ID must be provided.");
+
+  id = id.trim();
+
+  if (!ObjectId.isValid(id)) throw new Error ("Invalid id");
+
   const centerCollection = await centers();
   const center = await centerCollection.findOne({ _id: new ObjectId(id) });
   if (!center) throw new Error(`No center found with id ${id}.`);
   return center;
 };
 
+// TODO: implement filtering by borough, deviceType, operatingStatus
 const getCentersByFilter = async (filters = {}) => {
-  // TODO: implement filtering by borough, deviceType, operatingStatus
   const centerCollection = await centers();
+  let query = {};
+
+  if (filters.name && filters.name.trim()) {
+    query.location_name = {
+      $regex: filters.name.trim(),
+      $options: "i"
+    };
+  }
+
+  if (filters.borough && filters.borough.trim()) {
+    query.borough = filters.borough.trim();
+  }
+
+  if (filters.organizationName && filters.organizationName.trim()) {
+    query.operator_name = {
+      $regex: filters.organizationName.trim(),
+      $options: "i"
+    };
+  }
+
   return await centerCollection.find(filters).toArray();
 };
 
